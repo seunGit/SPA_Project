63362a7b21f4fcc17ae2016806a4137add2da9ca
seungit <minsg3669@gmail.com>
2023-11-28 23:51:12 +0900

    view 구현 및 렌더링

---
+++ frontend/static/js/pages/design.js
@@ -0,0 +1,8 @@
+export default class {
+    constructor() {
+        document.title = "design";
+    }
+    async getHtml() {
+        return `<h1>디자인 페이지 입니다.</h1>`;
+    }
+}
---
+++ frontend/static/js/pages/dev.js
@@ -0,0 +1,8 @@
+export default class {
+    constructor() {
+        document.title = "dev";
+    }
+    async getHtml() {
+        return `<h1>개발 페이지 입니다.</h1>`;
+    }
+}
