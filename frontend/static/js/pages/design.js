fb0c6125a3f319e88f0e2a50085933380220a005
seungit <minsg3669@gmail.com>
2023-11-29 11:23:19 +0900

    메뉴별 CSS 적용 및 HTML 추가

--- frontend/static/js/pages/design.js
+++ frontend/static/js/pages/design.js
@@ -5,17 +5,54 @@ export default class {
     async getHtml() {
         return `
         <div class="post_container">
-            <div class="post_name">디자인</div>
             <div class="post_list_container">
+            <div class="post_name">디자인</div>
+
                 <ul class="post_list">
                     <li>
                         <a href="#">
                             <div class="post_content">
-                                <img src="static/css/image/menu/menu_img_1.jpeg">
+                                <img src="static/css/image/menu/design_img/design_menu_img_1.png">
                                 <div class="menu_text">
                                     <span class="title">독이 되는 레퍼런스 활용법</span>
                                     <span class="content">매혹적이지만 독이 될 수 있는 레퍼런스. 어떻게 활용하는 것이 좋을까요?</span>
-                                    <span class="date">2023.11.27</span>
+                                    <span class="date">2023. 11. 27</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="post_content">
+                                <img src="static/css/image/menu/design_img/design_menu_img_2.png">
+                                <div class="menu_text">
+                                    <span class="title">효율적인 유저 리서치 환경을 만드는 리서치 플랫폼 팀, 들어보셨나요?</span>
+                                    <span class="content">토스팀에서 팀원들이 사용자를 더 쉽고, 빠르고, 편하게 만날 수 있도록 고군분투하고 있는 ‘리서치 플랫폼 팀’에 대해 소개해 보려고 해요. </span>
+                                    <span class="date">2023. 11. 23</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="post_content">
+                                <img src="static/css/image/menu/design_img/design_menu_img_3.png">
+                                <div class="menu_text">
+                                    <span class="title">완성 없는 이야기, 가입 과정 개선</span>
+                                    <span class="content">가입 완료율을 높이기 위해 고민하고 시도했던 것들을 이야기 해요.</span>
+                                    <span class="date">2023. 9. 12</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="post_content">
+                                <img src="static/css/image/menu/design_img/design_menu_img_4.png">
+                                <div class="menu_text">
+                                    <span class="title">인터랙션, 꼭 넣어야 해요?</span>
+                                    <span class="content">빠른 속도를 중요시하는 토스에서 어떻게 팀원들을 인터랙션에 공감하게 하고 슬릭한 제품을 만들어나갈 수 있었는지 소개할게요.</span>
+                                    <span class="date">2023. 9. 7</span>
                                 </div>
                             </div>
                         </a>
--- frontend/static/js/pages/dev.js
+++ frontend/static/js/pages/dev.js
@@ -11,11 +11,47 @@ export default class {
                     <li>
                         <a href="#">
                             <div class="post_content">
-                                <img src="static/css/image/menu/menu_img_1.jpeg">
+                                <img src="static/css/image/menu/dev_img/dev_menu_img_2.jpeg">
+                                <div class="menu_text">
+                                    <span class="title">인자가 많은 메서드는 왜 나쁠까?</span>
+                                    <span class="content">인자가 많은 메서드를 함께 리팩토링 하면서 코드 사용자 입장에서 코드 복잡성을 관리하는 방법을 알아봅니다.</span>
+                                    <span class="date">2023. 11. 29</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="post_content">
+                                <img src="static/css/image/menu/dev_img/dev_menu_img_1.jpeg">
                                 <div class="menu_text">
                                     <span class="title">Feign 코드 분석과 서버 성능 개선</span>
                                     <span class="content">Feign과 다중 스레드를 사용하는 과정에서 생긴 문제를 이해하고 성능 개선까지 한 경험을 공유해요.</span>
-                                    <span class="date">2023.11.22</span>
+                                    <span class="date">2023. 11. 22</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="post_content">
+                                <img src="static/css/image/menu/dev_img/dev_menu_img_3.png">
+                                <div class="menu_text">
+                                    <span class="title">null 리턴은 왜 나쁠까?</span>
+                                    <span class="content">코드 복잡성 관리 측면에서 의미를 축약한 표현의 문제와 해결 방법을 예제로 알아봐요.</span>
+                                    <span class="date">2023. 11. 8</span>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <div class="post_content">
+                                <img src="static/css/image/menu/dev_img/dev_menu_img_4.jpeg">
+                                <div class="menu_text">
+                                    <span class="title">웹에서 복잡한 퍼널 쉽게 관리하기</span>
+                                    <span class="content">토스페이먼츠 프론트엔드 챕터에서 웹에서 퍼널을 손쉽게 관리하기 위해 했던 고민과 해결 방법을 공유해요.</span>
+                                    <span class="date">2023. 10. 18</span>
                                 </div>
                             </div>
                         </a>
@@ -23,7 +59,6 @@ export default class {
                 </ul>
             </div>
         </div>
-
         `;
     }
 }
